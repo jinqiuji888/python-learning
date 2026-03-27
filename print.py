a6b8e878650433caa84fbba11907e5ba7757d44e
Jovi <jinqiuji888@gmail.com>
2026-03-27 17:44:47 +0900

    print.py
    print打印

---
+++ print.py
@@ -0,0 +1,16 @@
+print("hellow world")
+print(2+3）
+a=3
+b=4
+result=a+b
+print("a+b=",result)
+result=a-b
+print("a-b=",result)
+result=a*b
+print("a*b=",result)
+result=a/b
+print("a/b=",result)
+result=a**b
+print("a**b=",result)   #求3的4次方
+result=a//b
+print("ab=",result)     #求3/4的余数
